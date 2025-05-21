f1585ec553f57ea7bff64fc47d00f5f4994c9a35
Regenerate38 <mukunda.adhikari@outlook.com>
2025-05-22 00:52:24 +0545

    Added Raylib Drawing Main Loop.
    Finished Chip8 except for Sound

--- src/chip8.rs
+++ src/chip8.rs
@@ -2,15 +2,17 @@
 
 use rand::Rng;
 
-const memory_size: usize = 4096;
-const num_reg: usize = 16;
-const stack_size: usize = 16;
-const screen_width: usize = 64;
-const screen_height: usize = 64;
-const num_keys: usize = 16;
-const fontset_size: usize = 80;
+const MEMORY_SIZE: usize = 4096;
+const INSTRUCTION_LENGTH: u16 = 2;
+const NUM_REG: usize = 16;
+const STACK_SIZE: usize = 128;
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 64;
+const NUM_KEYS: usize = 16;
+const PROGRAM_LOAD_ADDRESS: usize = 0x200;
+const FONTSET_SIZE: usize = 80;
 
-const fontset: [u8; 80] = [
+const FONTSET: [u8; 80] = [
     0xF0, 0x90, 0x90, 0x90, 0xF0, //0
     0x20, 0x60, 0x20, 0x20, 0x70, //1
     0xF0, 0x10, 0xF0, 0x80, 0xF0, //2
@@ -30,19 +32,19 @@ const fontset: [u8; 80] = [
 ];
 
 pub struct Chip8 {
-    memory: [u8; memory_size],
+    memory: [u8; MEMORY_SIZE],
 
-    stack: [u16; stack_size],
+    stack: [u16; STACK_SIZE],
     sp: u16,
     // VF is carry flag in addition, no borrow flag in subtraction and sets in pixel collision in draw mode.
-    v: [u8; num_reg], //Registers
+    v: [u8; NUM_REG], //Registers
     ir: u16,          //Instruction register
     pc: u16,
     delay_timer: u8,
     sound_timer: u8,
     opcode: u16,
-    graphics: [bool; screen_width * screen_height],
-    keys: [bool; num_keys],
+    graphics: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    keys: [bool; NUM_KEYS],
 }
 
 impl Chip8 {
@@ -50,17 +52,17 @@ impl Chip8 {
         let mut new_chip = Self {
             sp: 0,
             ir: 0,
-            memory: [0; memory_size],
-            stack: [0; stack_size],
-            v: [0; num_reg],
-            keys: [false; num_keys],
-            graphics: [false; screen_width * screen_height],
+            memory: [0; MEMORY_SIZE],
+            stack: [0; STACK_SIZE],
+            v: [0; NUM_REG],
+            keys: [false; NUM_KEYS],
+            graphics: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
             delay_timer: 0,
             sound_timer: 0,
             opcode: 0,
-            pc: 0x200,
+            pc: PROGRAM_LOAD_ADDRESS as u16,
         };
-        new_chip.memory[..fontset_size].copy_from_slice(&fontset);
+        new_chip.memory[..FONTSET_SIZE].copy_from_slice(&FONTSET);
 
         new_chip
     }
@@ -70,13 +72,17 @@ impl Chip8 {
     }
     fn pop(&mut self) -> u16 {
         self.sp -= 1;
-        self.stack[(self.sp + 1) as usize]
+        self.stack[self.sp as usize]
     }
 
-    pub fn loadProgram(&mut self, data: &[u8]) {
+    pub fn loadProgram(&mut self, data: &[u8]) -> Result<usize, String> {
+        if data.len() > MEMORY_SIZE - PROGRAM_LOAD_ADDRESS {
+            return Err("ROM too big, aborting".to_string());
+        }
         let start = 0x200 as usize;
         let end = (0x200 as usize) + data.len();
         self.memory[start..end].copy_from_slice(data);
+        Ok(data.len())
     }
 
     pub fn get_display(&self) -> &[bool] {
@@ -84,13 +90,22 @@ impl Chip8 {
     }
     pub fn keypress(&mut self, idx: usize, pressed: bool) {
         self.keys[idx] = pressed;
+        println!(
+            "Key at index {} is {}",
+            idx,
+            if pressed { "pressed" } else { "released" }
+        );
     }
 
-    fn executeCycle(&mut self) {
+    pub fn is_pixel_set(&self, x: usize, y: usize) -> bool {
+        self.graphics[x + SCREEN_HEIGHT * y]
+    }
+    pub fn executeCycle(&mut self) {
         //fetch opcode
         let higher_byte = self.memory[self.pc as usize] as u16;
         let lower_byte = self.memory[(self.pc + 1) as usize] as u16;
         self.opcode = higher_byte << 8 | lower_byte;
+        self.pc += INSTRUCTION_LENGTH;
 
         //decode and execute opcode
         let d1 = (self.opcode & 0xF000) >> 12;
@@ -101,7 +116,7 @@ impl Chip8 {
             // NOP
             (0, 0, 0, 0) => {}
             // CLEAR
-            (0, 0, 0xE, 0) => self.graphics = [false; screen_width * screen_height],
+            (0, 0, 0xE, 0) => self.graphics = [false; SCREEN_WIDTH * SCREEN_HEIGHT],
             // RET
             (0, 0, 0xE, 0xE) => {
                 let return_addr = self.pop();
@@ -123,7 +138,7 @@ impl Chip8 {
                 let nn = (self.opcode & 0xFF) as u8;
                 let x = d2 as usize;
                 if (self.v[x] == nn) {
-                    self.pc += 2;
+                    self.pc += INSTRUCTION_LENGTH;
                 }
             }
             // SKIP if VX =/= NN
@@ -131,7 +146,7 @@ impl Chip8 {
                 let nn = (self.opcode & 0xFF) as u8;
                 let x = d2 as usize;
                 if (self.v[x] != nn) {
-                    self.pc += 2;
+                    self.pc += INSTRUCTION_LENGTH;
                 }
             }
             // SKIP if VX == VY
@@ -139,7 +154,7 @@ impl Chip8 {
                 let x = d2 as usize;
                 let y = d3 as usize;
                 if (self.v[x] == self.v[y]) {
-                    self.pc += 2;
+                    self.pc += INSTRUCTION_LENGTH;
                 }
             }
             // SET VX = NN
@@ -224,7 +239,7 @@ impl Chip8 {
                 let x = d2 as usize;
                 let y = d3 as usize;
                 if (self.v[x] != self.v[y]) {
-                    self.pc += 2;
+                    self.pc += INSTRUCTION_LENGTH;
                 }
             }
             // SETS IR = NNN
@@ -248,15 +263,117 @@ impl Chip8 {
             (0xE, _, 0x9, 0xE) => {
                 let x = d2 as usize;
                 if (self.keys[self.v[x] as usize] == true) {
-                    self.pc += 2
+                    self.pc += INSTRUCTION_LENGTH
                 }
             }
             // SKIP if key[VX] isn't pressed
             (0xE, _, 0xA, 0x1) => {
                 let x = d2 as usize;
                 if (self.keys[self.v[x] as usize] == false) {
-                    self.pc += 2
+                    self.pc += INSTRUCTION_LENGTH
                 }
+            }
+            // DRAW. this was hard
+            (0xD, _, _, _) => {
+                let x = self.v[d2 as usize] as u16;
+                let y = self.v[d3 as usize] as u16;
+                let n = d4 as usize;
+
+                let mut flipped = false;
+
+                for line_no in 0..n {
+                    let address = self.ir + line_no as u16;
+                    let pixels = self.memory[address as usize];
+
+                    for cell in 0..8 {
+                        if (pixels & (0b1000_0000 >> cell)) != 0 {
+                            let x_draw = (x + cell) as usize % SCREEN_WIDTH;
+                            let y_draw = (y + line_no as u16) as usize % SCREEN_HEIGHT;
+                            let index = x_draw + SCREEN_HEIGHT * y_draw;
+                            flipped = flipped | self.get_display()[index];
+                            self.graphics[index] ^= true;
+                        }
+                    }
+                }
+                if flipped {
+                    self.v[0xf] = 1;
+                } else {
+                    self.v[0xf] = 0;
+                }
+            }
+            // SET VX = DT
+            (0xF, _, 0x0, 0x7) => {
+                let x = d2 as usize;
+                self.v[x] = self.delay_timer;
+            }
+            // WAIT KEY
+            (0xF, _, 0x0, 0xA) => {
+                let x = d2 as usize;
+                let mut key_pressed = false;
+                for i in 0..self.keys.len() {
+                    if self.keys[i] {
+                        self.v[x] = i as u8;
+                        key_pressed = true;
+                        break;
+                    }
+                }
+                if !key_pressed {
+                    self.pc -= INSTRUCTION_LENGTH;
+                    println!("Waiting for key input...");
+                }
+            }
+            // SET DT = VX
+            (0xf, _, 0x1, 0x5) => {
+                let x = d2 as usize;
+                self.delay_timer = self.v[x];
+            }
+            // SET ST = VX
+            (0xf, _, 0x1, 0x8) => {
+                let x = d2 as usize;
+                self.sound_timer = self.v[x];
+            }
+            // ADD IR, VX
+            (0xf, _, 0x1, 0xe) => {
+                let x = d2 as usize;
+                self.ir = self.ir.wrapping_add(self.v[x] as u16);
+            }
+            // MOV IR, SPRITE[VX]. USING FONT SPRITES HERE
+            (0xf, _, 0x2, 0x9) => {
+                let x = d2 as usize;
+                self.ir = self.v[x] as u16 * 5;
+            }
+            // MOV RAM, VX
+            (0xf, _, 0x5, 0x5) => {
+                let x = d2 as usize;
+                // for i in (self.ir as usize)..(self.ir as usize + x) {
+                //     self.memory[i] = self.v[i]
+                // }
+                for i in 0..x {
+                    self.memory[(self.ir as usize) + i] = self.v[i];
+                }
+            }
+            // MOV VX, RAM
+            (0xf, _, 0x6, 0x5) => {
+                let x = d2 as usize;
+                // for i in (self.ir as usize)..(self.ir as usize + x) {
+                //     self.v[i] = self.memory[i]
+                // }
+                for i in 0..x {
+                    self.v[i] = self.memory[(self.ir as usize) + i];
+                }
+            }
+            // BCD STUFF
+            (0xF, _, 0x3, 0x3) => {
+                let x = d2 as usize;
+                let vx = self.v[x] as f32;
+
+                let ones = (vx % 10.0) as u8;
+                let tens = ((vx / 10.0) % 10.0).floor() as u8;
+                let hundreds = ((vx / 100.0) % 10.0).floor() as u8;
+
+                self.memory[self.ir as usize] = hundreds;
+                self.memory[(self.ir + 1) as usize] = tens;
+                self.memory[(self.ir + 2) as usize] = ones;
             }
 
             _ => {}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,99 @@
 #![allow(non_snake_case)]
 
 use raylib::prelude::*;
+use std::{env, fs};
 
 mod chip8;
 use chip8::{Chip8, *};
 
+const TICKS_PER_FRAME: usize = 10;
 fn main() {
-    let mychip8 = Chip8::new();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("{} Enter Path to ROM", &args[0]);
+        std::process::exit(0)
+    }
 
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+    std::process::exit(match run(&args[1]) {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("Error {:?}", err);
+            1
+        }
+    });
+}
 
+fn run(rom_path: &str) -> Result<(), String> {
+    println!("Initializing Raylib");
+    let window_title = format!("Chip 8: {rom_path}");
+    // Initializing Raylib and Chip8
+    let mut mychip8 = Chip8::new();
+    let (mut rl, thread) = raylib::init().size(640, 640).title(&window_title).build();
+
+    println!("Loading ROM {rom_path}");
+    let rom = fs::read(rom_path).map_err(|e| format!("Cannot read ROM: {}", e))?;
+    let count = mychip8.loadProgram(&rom)?;
+    println!("Loaded the program: {:?} bytes", count);
+
+    rl.set_target_fps(60);
     while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+        let key = rl.get_key_pressed();
+        if key == Some(KeyboardKey::KEY_ESCAPE) {
+            break;
+        }
 
-        d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        let keys_to_check = [
+            (KeyboardKey::KEY_ONE, 1),
+            (KeyboardKey::KEY_TWO, 2),
+            (KeyboardKey::KEY_THREE, 3),
+            (KeyboardKey::KEY_FOUR, 12),
+            (KeyboardKey::KEY_Q, 4),
+            (KeyboardKey::KEY_W, 5),
+            (KeyboardKey::KEY_E, 6),
+            (KeyboardKey::KEY_R, 13),
+            (KeyboardKey::KEY_A, 7),
+            (KeyboardKey::KEY_S, 8),
+            (KeyboardKey::KEY_D, 9),
+            (KeyboardKey::KEY_F, 14),
+            (KeyboardKey::KEY_Z, 10),
+            (KeyboardKey::KEY_X, 0),
+            (KeyboardKey::KEY_C, 11),
+            (KeyboardKey::KEY_V, 15),
+        ];
+
+        for (key, value) in keys_to_check {
+            if rl.is_key_pressed(key) {
+                mychip8.keypress(value, true);
+            }
+            if rl.is_key_released(key) {
+                mychip8.keypress(value, false);
+            }
+        }
+
+        for _ in 0..TICKS_PER_FRAME {
+            mychip8.executeCycle();
+        }
+        let mut d = rl.begin_drawing(&thread);
+        draw_frame(&mychip8, &mut d);
+    }
+    Ok(())
+}
+
+fn draw_frame(chip8: &chip8::Chip8, d: &mut RaylibDrawHandle) {
+    d.clear_background(Color::BLACK);
+    let scale = 10;
+
+    for y in 0..chip8::SCREEN_HEIGHT {
+        for x in 0..chip8::SCREEN_WIDTH {
+            if chip8.is_pixel_set(x, y) {
+                d.draw_rectangle(
+                    x as i32 * scale,
+                    y as i32 * scale,
+                    scale,
+                    scale,
+                    Color::WHITE,
+                );
+            }
+        }
     }
 }
